65a21100034ebc057b49e9c3a449e7517bad18af
my-scala-steward <contact.110416+scalasteward@gmail.com>
2022-03-30 18:41:55 +0000

    Reformat with scalafmt 3.4.3

--- petit/shared/src/main/scala/dev/i10416/petit/static/SiteMap.scala
+++ petit/shared/src/main/scala/dev/i10416/petit/static/SiteMap.scala
@@ -11,16 +11,16 @@ object SiteMap {
         |<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         |  xmlns:xhtml="http://www.w3.org/1999/xhtml">
         | ${paths.map { path =>
-      s"""|
+         s"""|
              |  <url>
              |    <loc> ${host}${baseDirectory
-           .fold("")(dir => "/dir")}/$path/index.html</loc>
+              .fold("")(dir => "/dir")}/$path/index.html</loc>
              |    <!-- <lastmod>???</lastmod> -->
              |    <!-- <changefreq></changefreq>  -->
              |    <!-- <priority></priority>  -->
              |  </url>
              |""".stripMargin
-    }.mkString}
+       }.mkString}
         |</urlset>
         |""".stripMargin
 
