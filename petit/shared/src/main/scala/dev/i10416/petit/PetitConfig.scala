b081c6fae3f8ec61c2805478a605e9cbb7a230af
my-scala-steward <contact.110416+scalasteward@gmail.com>
2022-03-30 18:31:57 +0000

    Reformat with scalafmt 3.0.8

--- petit/shared/src/main/scala/dev/i10416/petit/PetitConfig.scala
+++ petit/shared/src/main/scala/dev/i10416/petit/PetitConfig.scala
@@ -1,14 +1,14 @@
 package dev.i10416.petit
 
 case class PetitConfig(
-  metadata:SiteMetaData
+    metadata: SiteMetaData
 )
 
 case class SiteMetaData(
     baseURL: String = "localhost",
     title: String = "",
     description: String = "",
-    language:Option[String] = None,
-    admins:List[String] = Nil,
+    language: Option[String] = None,
+    admins: List[String] = Nil,
     copyWrightOwners: List[String] = Nil
 )
--- petit/shared/src/main/scala/dev/i10416/petit/static/SiteMap.scala
+++ petit/shared/src/main/scala/dev/i10416/petit/static/SiteMap.scala
@@ -1,26 +1,27 @@
 package dev.i10416.petit.static
 
-
 object SiteMap {
 
-  def apply(host:String = "localhost",baseDirectory:Option[String]=None,paths:List[String]=Nil)= 
+  def apply(
+      host: String = "localhost",
+      baseDirectory: Option[String] = None,
+      paths: List[String] = Nil
+  ) =
     s"""|
         |<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         |  xmlns:xhtml="http://www.w3.org/1999/xhtml">
-        | ${paths.map{ path =>
-         s"""|
+        | ${paths.map { path =>
+      s"""|
              |  <url>
-             |    <loc> ${host}${baseDirectory.fold("")(dir=>"/dir")}/$path/index.html</loc>
+             |    <loc> ${host}${baseDirectory
+           .fold("")(dir => "/dir")}/$path/index.html</loc>
              |    <!-- <lastmod>???</lastmod> -->
              |    <!-- <changefreq></changefreq>  -->
              |    <!-- <priority></priority>  -->
              |  </url>
              |""".stripMargin
-            }.mkString
-          }
+    }.mkString}
         |</urlset>
         |""".stripMargin
-  
 
-    
 }
